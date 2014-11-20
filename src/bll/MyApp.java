8660886f2d6c219e54ab8885d0c8421efa65dd35
jwhistine <justinwhistine@gmail.com>
2014-11-20 14:25:06 -0700

    The class example for logging

--- src/bll/MyApp.java
+++ src/bll/MyApp.java
@@ -5,6 +5,11 @@
  */
 package bll;
 
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 import util.FileHandler;
 
 /**
@@ -12,6 +17,8 @@ import util.FileHandler;
  * @author sburton
  */
 public class MyApp {
+    
+    static final Logger logger = Logger.getLogger(MyApp.class.getName());
 
     /**
      * @param args the command line arguments
@@ -21,7 +28,28 @@ public class MyApp {
     }
     
     public void run() {
+        try{ 
+            LogManager manager = LogManager.getLogManager();
+            manager.readConfiguration(manager.getClass().getResourceAsStream("/resources/logging.properties"));
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        
+        
+        try {
+            Handler fileHandler = new java.util.logging.FileHandler("log.txt");
+            fileHandler.setLevel(Level.FINE);
+            fileHandler.setFormatter(new SimpleFormatter());
+            
+            logger.addHandler(fileHandler);
+            logger.setLevel(Level.FINE);
+            
+        } catch (Exception ex){
+            ex.printStackTrace();
+        }
         // TODO: Log a "fine" message here, saying: "starting program"
+        
+        logger.fine("Starting program");
         
         FileHandler handler = new FileHandler();
         
@@ -29,6 +57,7 @@ public class MyApp {
         
         if (firstLine == null) {
             // TODO: Log a "severe" message here, saying "Could not read first line"
+            logger.severe("Could not read first line");
         }
     }
     
--- src/util/FileHandler.java
+++ src/util/FileHandler.java
@@ -3,6 +3,7 @@ package util;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.logging.Logger;
 
 /**
  *
@@ -10,8 +11,11 @@ import java.io.IOException;
  */
 public class FileHandler {
     
+    static final Logger logger = Logger.getLogger(FileHandler.class.getName());
+    
     public String loadFirstLine(String file) {
         // TODO: Log a "fine" message here saying: "Starting loadFirstLine"
+        logger.fine("Starting loadFirstLine");
         
         String line = null;
         
@@ -23,6 +27,7 @@ public class FileHandler {
         } catch (IOException ex) {
             // TODO: Log a "severe" message here saying: "Could not load file."
             // and include the exception with it.
+            logger.severe("Could not load file");
         }
         
         return line;
